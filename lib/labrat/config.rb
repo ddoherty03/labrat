1754e49056b9a007bfb92f51107e843e237df551
Daniel E. Doherty <ded-law@ddoherty.net>
2021-08-13 10:07:13 -0500

    Test and fix non-xdg user config reading.

--- lib/labrat/config.rb
+++ lib/labrat/config.rb
@@ -103,53 +103,54 @@ module Labrat
       configs
     end
 
-    # Return the absolute path names of all XDG user config files for app_name
-    # with the basename variants of base. Return the lowest priority files
-    # first, highest last. Prefix the search locations with dir_prefix if
-    # given.
-    def self.find_xdg_user_config_files(app_name, base, dir_prefix)
+    # Return the absolute path name of any XDG user config files for app_name
+    # with the basename variants of base. The XDG_CONFIG_HOME environment
+    # variable for the user configs is intended to be the name of a single xdg
+    # config directory, not a list of colon-separated directories as for the
+    # system config. Return the name of a config file for this app in
+    # XDG_CONFIG_HOME (or ~/.config by default).  Prefix the search location
+    # with dir_prefix if given.
+    def self.find_xdg_user_config_file(app_name, base, dir_prefix)
       dir_prefix ||= ''
       base ||= (base&.strip || app_name)
-      configs = []
-      xdg_search_dirs = ENV['XDG_CONFIG_HOME']&.split(':')&.reverse || ['~/.config']
-      xdg_search_dirs.each do |dir|
-        dir = File.expand_path(File.join(dir, app_name))
-        dir = File.join(dir_prefix, dir) unless dir_prefix.strip.empty?
-        next unless Dir.exist?(dir)
+      xdg_search_dir = ENV['XDG_CONFIG_HOME'] || ['~/.config']
+      dir = File.expand_path(File.join(xdg_search_dir, app_name))
+      dir = File.join(dir_prefix, dir) unless dir_prefix.strip.empty?
+      return nil unless Dir.exist?(dir)
 
-        base_candidates = ["#{base}", "#{base}.yml", "#{base}.yaml", "#{base}.cfg", "#{base}.config"]
-        config = base_candidates.find { |b| File.readable?(File.join(dir, b)) }
-        configs = [ File.join(dir, config) ] if config
+      base_candidates = ["#{base}", "#{base}.yml", "#{base}.yaml", "#{base}.cfg", "#{base}.config"]
+      config_fname = base_candidates.find { |b| File.readable?(File.join(dir, b)) }
+      if config_fname
+        File.join(dir, config_fname)
       end
-      configs
     end
 
     # Return the absolute path names of all "classic" system config files for
     # app_name with the basename variants of base. Return the lowest priority
     # files first, highest last.  Prefix the search locations with dir_prefix if
     # given.
-    def self.find_classic_user_config_files(app_name, base, dir_prefix)
+    def self.find_classic_user_config_file(app_name, base, dir_prefix)
       dir_prefix ||= ''
       base ||= (base&.strip || app_name)
-      base_candidates = ["#{base}" "#{base}.yml", "#{base}.yaml", "#{base.cfg}", "#{base}.config"]
-      configs = []
+      config_fname = nil
       env_config = ENV["#{app_name.upcase}_CONFIG"]
       if env_config && File.readable?((config = File.join(dir_prefix, File.expand_path(env_config))))
-        configs = [ config ]
-      elsif File.readable?(config = (File.join(dir_prefix, File.expand_path("~/.#{app_name}rc"))))
-        configs = [ config ]
+        config_fname = config
       elsif Dir.exist?(config_dir = (File.join(dir_prefix, File.expand_path("~/.#{app_name}"))))
-        config = base_candidates.find { |b| File.readable?(File.join(config_dir, b)) }
-        configs = [ config ]
+        base_candidates = ["config.yml", "config.yaml", "config"]
+        base = base_candidates.find { |b| File.readable?(File.join(config_dir, b)) }
+        config_fname = File.join(config_dir, base)
+      elsif Dir.exist?(config_dir = (File.join(dir_prefix, File.expand_path('~/'))))
+        base_candidates = [".#{app_name}", ".#{app_name}rc", ".#{app_name}.yml", ".#{app_name}.yaml",
+                           ".#{app_name}.cfg", ".#{app_name}.config"]
+        base = base_candidates.find { |b| File.readable?(File.join(config_dir, b)) }
+        config_fname = File.join(config_dir, base)
       end
-      configs
+      config_fname
     end
 
     # Merge the settings from config files with the name config_name from the
-    # given directories, dirs, into the given Options object.  Per the XDG
-    # specification, directories listed first are the most important, so we
-    # merge them in reverse order so that earlier-listed config directories
-    # override later-listed ones.
+    # given directories, dirs, into the given Options object.
     def self.merge_configs_from(files = [], hash)
       files.each do |f|
         if File.readable?(f)
--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -20,14 +20,16 @@ RSpec.describe Config do
   end
 
   def setup_test_file(path, content)
+    path = File.expand_path(path)
     test_path = File.join(SANDBOX_DIR, path)
     dir_part = File.dirname(test_path)
     FileUtils.mkdir_p(dir_part) unless Dir.exist?(dir_part)
     File.write(test_path, content)
   end
 
-  let(:yaml_str) do
-    <<~YAML
+  describe 'Basic YAML reading' do
+    let(:yaml_str) do
+      <<~YAML
       ---
       doe: "a deer, a female deer"
       ray: "a drop of golden sun"
@@ -48,15 +50,17 @@ RSpec.describe Config do
           location: "a pear tree"
         turtle-doves: two
     YAML
+    end
+
+    it 'can read a yaml string' do
+      struct = YAML.load(yaml_str)
+      expect(struct.keys).to include('doe')
+    end
   end
 
-  it 'can read a yaml string' do
-    struct = YAML.load(yaml_str)
-    expect(struct.keys).to include('doe')
-  end
-
-  it 'can read from an xdg system config file' do
-    config_yml = <<~YAML
+  describe 'Reading XDG config files' do
+    it 'can read from an xdg system config file' do
+      config_yml = <<~YAML
       width: 33mm
       height: 101mm
       delta_x: -4mm
@@ -64,52 +68,52 @@ RSpec.describe Config do
       nlsep: '%%'
       printer: seiko3
     YAML
-    setup_test_file('/etc/xdg/labrat/config.yml', config_yml)
-    hsh = Config.read('labrat', xdg: true, dir_prefix: SANDBOX_DIR)
-    op = ArgParser.new.from_hash(hsh)
-    expect(op.width).to be_within(EPS).of(33 * MM)
-    expect(op.height).to be_within(EPS).of(101 * MM)
-    expect(op.delta_x).to be_within(EPS).of(-4 * MM)
-    expect(op.delta_y).to be_within(EPS).of(1 * CM)
-    expect(op.nlsep).to eq('%%')
-    expect(op.printer).to eq('seiko3')
-  end
+      setup_test_file('/etc/xdg/labrat/config.yml', config_yml)
+      hsh = Config.read('labrat', xdg: true, dir_prefix: SANDBOX_DIR)
+      op = ArgParser.new.from_hash(hsh)
+      expect(op.width).to be_within(EPS).of(33 * MM)
+      expect(op.height).to be_within(EPS).of(101 * MM)
+      expect(op.delta_x).to be_within(EPS).of(-4 * MM)
+      expect(op.delta_y).to be_within(EPS).of(1 * CM)
+      expect(op.nlsep).to eq('%%')
+      expect(op.printer).to eq('seiko3')
+    end
 
-  it 'can read from an XDG_CONFIG_DIRS xdg system directory config file' do
-    config_yml = <<~YAML
+    it 'can read from an XDG_CONFIG_DIRS xdg system directory config file' do
+      config_yml = <<~YAML
       width: 33mm
       height: 101mm
       delta_x: -4mm
       delta_y: 1cm
       nlsep: '%%'
     YAML
-    setup_test_file('/lib/junk/labrat/config.yml', config_yml)
-    config2_yml = <<~YAML
+      setup_test_file('/lib/junk/labrat/config.yml', config_yml)
+      config2_yml = <<~YAML
       width: 3cm
       height: 10cm
       delta_x: -4pt
       printer: dymo4
       label: avery1234
     YAML
-    setup_test_file('/lib/lowjunk/labrat/config.yml', config2_yml)
+      setup_test_file('/lib/lowjunk/labrat/config.yml', config2_yml)
 
-    # The first directory in the ENV variable list should take precedence.
-    ENV['XDG_CONFIG_DIRS'] = "/lib/junk:#{ENV['XDG_CONFIG_DIRS']}:/lib/lowjunk"
-    hsh = Config.read('labrat', xdg: true, dir_prefix: SANDBOX_DIR)
-    op = ArgParser.new.from_hash(hsh)
-    expect(op.width).to be_within(EPS).of(33 * MM)
-    expect(op.height).to be_within(EPS).of(101 * MM)
-    expect(op.delta_x).to be_within(EPS).of(-4 * MM)
-    expect(op.delta_y).to be_within(EPS).of(1 * CM)
-    expect(op.nlsep).to eq('%%')
-    # Since these were not specified in the high-priority config, but were in
-    # the low-priority config, they get set.
-    expect(op.printer).to eq('dymo4')
-    expect(op.label).to eq('avery1234')
-  end
+      # The first directory in the ENV variable list should take precedence.
+      ENV['XDG_CONFIG_DIRS'] = "/lib/junk:#{ENV['XDG_CONFIG_DIRS']}:/lib/lowjunk"
+      hsh = Config.read('labrat', xdg: true, dir_prefix: SANDBOX_DIR)
+      op = ArgParser.new.from_hash(hsh)
+      expect(op.width).to be_within(EPS).of(33 * MM)
+      expect(op.height).to be_within(EPS).of(101 * MM)
+      expect(op.delta_x).to be_within(EPS).of(-4 * MM)
+      expect(op.delta_y).to be_within(EPS).of(1 * CM)
+      expect(op.nlsep).to eq('%%')
+      # Since these were not specified in the high-priority config, but were in
+      # the low-priority config, they get set.
+      expect(op.printer).to eq('dymo4')
+      expect(op.label).to eq('avery1234')
+    end
 
-  it 'can read from an xdg system ENV-specified config file' do
-    config_yml = <<~YAML
+    it 'can read from an xdg system ENV-specified config file' do
+      config_yml = <<~YAML
       width: 33mm
       height: 101mm
       delta_x: -4mm
@@ -117,20 +121,20 @@ RSpec.describe Config do
       nlsep: '%%'
       printer: seiko3
     YAML
-    ENV['LABRAT_SYS_CONFIG'] = '/etc/labrat.yml'
-    setup_test_file(ENV['LABRAT_SYS_CONFIG'], config_yml)
-    hsh = Config.read('labrat', xdg: true, dir_prefix: SANDBOX_DIR)
-    op = ArgParser.new.from_hash(hsh)
-    expect(op.width).to be_within(EPS).of(33 * MM)
-    expect(op.height).to be_within(EPS).of(101 * MM)
-    expect(op.delta_x).to be_within(EPS).of(-4 * MM)
-    expect(op.delta_y).to be_within(EPS).of(1 * CM)
-    expect(op.nlsep).to eq('%%')
-    expect(op.printer).to eq('seiko3')
-  end
+      ENV['LABRAT_SYS_CONFIG'] = '/etc/labrat.yml'
+      setup_test_file(ENV['LABRAT_SYS_CONFIG'], config_yml)
+      hsh = Config.read('labrat', xdg: true, dir_prefix: SANDBOX_DIR)
+      op = ArgParser.new.from_hash(hsh)
+      expect(op.width).to be_within(EPS).of(33 * MM)
+      expect(op.height).to be_within(EPS).of(101 * MM)
+      expect(op.delta_x).to be_within(EPS).of(-4 * MM)
+      expect(op.delta_y).to be_within(EPS).of(1 * CM)
+      expect(op.nlsep).to eq('%%')
+      expect(op.printer).to eq('seiko3')
+    end
 
-  it 'can read from an xdg user config file' do
-    config_yml = <<~YAML
+    it 'can read from an xdg user config file' do
+      config_yml = <<~YAML
       width: 33mm
       height: 101mm
       delta_x: -4mm
@@ -138,19 +142,19 @@ RSpec.describe Config do
       nlsep: '%%'
       printer: seiko3
     YAML
-    setup_test_file('/home/ded/.config/labrat/config.yml', config_yml)
-    hsh = Config.read('labrat', xdg: true, dir_prefix: SANDBOX_DIR)
-    op = ArgParser.new.from_hash(hsh)
-    expect(op.width).to be_within(EPS).of(33 * MM)
-    expect(op.height).to be_within(EPS).of(101 * MM)
-    expect(op.delta_x).to be_within(EPS).of(-4 * MM)
-    expect(op.delta_y).to be_within(EPS).of(1 * CM)
-    expect(op.nlsep).to eq('%%')
-    expect(op.printer).to eq('seiko3')
-  end
+      setup_test_file('/home/ded/.config/labrat/config.yml', config_yml)
+      hsh = Config.read('labrat', xdg: true, dir_prefix: SANDBOX_DIR)
+      op = ArgParser.new.from_hash(hsh)
+      expect(op.width).to be_within(EPS).of(33 * MM)
+      expect(op.height).to be_within(EPS).of(101 * MM)
+      expect(op.delta_x).to be_within(EPS).of(-4 * MM)
+      expect(op.delta_y).to be_within(EPS).of(1 * CM)
+      expect(op.nlsep).to eq('%%')
+      expect(op.printer).to eq('seiko3')
+    end
 
-  it 'can read from an xdg ENV-specified user config file' do
-    config_yml = <<~YAML
+    it 'can read from an xdg ENV-specified user config file' do
+      config_yml = <<~YAML
       width: 33mm
       height: 101mm
       delta_x: -4mm
@@ -158,20 +162,20 @@ RSpec.describe Config do
       nlsep: '%%'
       printer: seiko3
     YAML
-    ENV['LABRAT_CONFIG'] = '/home/ded/.labrc'
-    setup_test_file(ENV['LABRAT_CONFIG'], config_yml)
-    hsh = Config.read('labrat', xdg: true, dir_prefix: SANDBOX_DIR)
-    op = ArgParser.new.from_hash(hsh)
-    expect(op.width).to be_within(EPS).of(33 * MM)
-    expect(op.height).to be_within(EPS).of(101 * MM)
-    expect(op.delta_x).to be_within(EPS).of(-4 * MM)
-    expect(op.delta_y).to be_within(EPS).of(1 * CM)
-    expect(op.nlsep).to eq('%%')
-    expect(op.printer).to eq('seiko3')
-  end
+      ENV['LABRAT_CONFIG'] = '/home/ded/.labrc'
+      setup_test_file(ENV['LABRAT_CONFIG'], config_yml)
+      hsh = Config.read('labrat', xdg: true, dir_prefix: SANDBOX_DIR)
+      op = ArgParser.new.from_hash(hsh)
+      expect(op.width).to be_within(EPS).of(33 * MM)
+      expect(op.height).to be_within(EPS).of(101 * MM)
+      expect(op.delta_x).to be_within(EPS).of(-4 * MM)
+      expect(op.delta_y).to be_within(EPS).of(1 * CM)
+      expect(op.nlsep).to eq('%%')
+      expect(op.printer).to eq('seiko3')
+    end
 
-  it 'can merge an xdg user config into an xdg system config file' do
-    sys_config_yml = <<~YAML
+    it 'can merge an xdg user config into an xdg system config file' do
+      sys_config_yml = <<~YAML
       width: 33mm
       height: 101mm
       delta_x: -4mm
@@ -179,19 +183,154 @@ RSpec.describe Config do
       nlsep: '%%'
       printer: seiko3
     YAML
-    setup_test_file('/etc/xdg/labrat/config.yml', sys_config_yml)
-    usr_config_yml = <<~YAML
+      setup_test_file('/etc/xdg/labrat/config.yml', sys_config_yml)
+      usr_config_yml = <<~YAML
       height: 102mm
       delta_x: -3mm
     YAML
-    setup_test_file('/home/ded/.config/labrat/config.yml', usr_config_yml)
-    hsh = Config.read('labrat', xdg: true, dir_prefix: SANDBOX_DIR)
-    op = ArgParser.new.from_hash(hsh)
-    expect(op.width).to be_within(EPS).of(33 * MM)
-    expect(op.height).to be_within(EPS).of(102 * MM)
-    expect(op.delta_x).to be_within(EPS).of(-3 * MM)
-    expect(op.delta_y).to be_within(EPS).of(1 * CM)
-    expect(op.nlsep).to eq('%%')
-    expect(op.printer).to eq('seiko3')
+      setup_test_file('/home/ded/.config/labrat/config.yml', usr_config_yml)
+      hsh = Config.read('labrat', xdg: true, dir_prefix: SANDBOX_DIR)
+      op = ArgParser.new.from_hash(hsh)
+      expect(op.width).to be_within(EPS).of(33 * MM)
+      expect(op.height).to be_within(EPS).of(102 * MM)
+      expect(op.delta_x).to be_within(EPS).of(-3 * MM)
+      expect(op.delta_y).to be_within(EPS).of(1 * CM)
+      expect(op.nlsep).to eq('%%')
+      expect(op.printer).to eq('seiko3')
+    end
+
+    it 'can read from an XDG_CONFIG_HOME xdg user directory config file' do
+      config_yml = <<~YAML
+      width: 33mm
+      height: 101mm
+      delta_x: -4mm
+      delta_y: 1cm
+      nlsep: '%%'
+    YAML
+      setup_test_file('~/.foncig/labrat/config.yml', config_yml)
+
+      # The first directory in the ENV variable list should take precedence.
+      ENV['XDG_CONFIG_HOME'] = "~/.foncig"
+      hsh = Config.read('labrat', xdg: true, dir_prefix: SANDBOX_DIR)
+      op = ArgParser.new.from_hash(hsh)
+      expect(op.width).to be_within(EPS).of(33 * MM)
+      expect(op.height).to be_within(EPS).of(101 * MM)
+      expect(op.delta_x).to be_within(EPS).of(-4 * MM)
+      expect(op.delta_y).to be_within(EPS).of(1 * CM)
+      expect(op.nlsep).to eq('%%')
+    end
+  end
+
+  describe 'Reading classic config files' do
+    it 'can read from an classic system config file' do
+      config_yml = <<~YAML
+      width: 33mm
+      height: 101mm
+      delta_x: -4mm
+      delta_y: 1cm
+      nlsep: '%%'
+      printer: seiko3
+    YAML
+      ENV['LABRAT_SYS_CONFIG'] = '/etc/labrat/config.yaml'
+      setup_test_file(ENV['LABRAT_SYS_CONFIG'], config_yml)
+      hsh = Config.read('labrat', xdg: false, dir_prefix: SANDBOX_DIR)
+      op = ArgParser.new.from_hash(hsh)
+      expect(op.width).to be_within(EPS).of(33 * MM)
+      expect(op.height).to be_within(EPS).of(101 * MM)
+      expect(op.delta_x).to be_within(EPS).of(-4 * MM)
+      expect(op.delta_y).to be_within(EPS).of(1 * CM)
+      expect(op.nlsep).to eq('%%')
+      expect(op.printer).to eq('seiko3')
+    end
+
+    it "can read from an classic user config file in ENV['LABRAT_CONFIG']" do
+      config_yml = <<~YAML
+      width: 33mm
+      height: 101mm
+      delta_x: -4mm
+      delta_y: 1cm
+      nlsep: '%%'
+      printer: seiko3
+    YAML
+      ENV['LABRAT_CONFIG'] = '~/junk/random/lr.y'
+      setup_test_file(ENV['LABRAT_CONFIG'], config_yml)
+      hsh = Config.read('labrat', xdg: false, dir_prefix: SANDBOX_DIR)
+      op = ArgParser.new.from_hash(hsh)
+      expect(op.width).to be_within(EPS).of(33 * MM)
+      expect(op.height).to be_within(EPS).of(101 * MM)
+      expect(op.delta_x).to be_within(EPS).of(-4 * MM)
+      expect(op.delta_y).to be_within(EPS).of(1 * CM)
+      expect(op.nlsep).to eq('%%')
+      expect(op.printer).to eq('seiko3')
+    end
+
+    it 'can read from an classic user rc-style config file in HOME' do
+      config_yml = <<~YAML
+      width: 33mm
+      height: 101mm
+      delta_x: -4mm
+      delta_y: 1cm
+      nlsep: '%%'
+      printer: seiko3
+    YAML
+      setup_test_file('~/.labratrc', config_yml)
+      hsh = Config.read('labrat', xdg: false, dir_prefix: SANDBOX_DIR)
+      op = ArgParser.new.from_hash(hsh)
+      expect(op.width).to be_within(EPS).of(33 * MM)
+      expect(op.height).to be_within(EPS).of(101 * MM)
+      expect(op.delta_x).to be_within(EPS).of(-4 * MM)
+      expect(op.delta_y).to be_within(EPS).of(1 * CM)
+      expect(op.nlsep).to eq('%%')
+      expect(op.printer).to eq('seiko3')
+    end
+
+    it 'can read from an classic ~/.labrat config dir in HOME' do
+      config_yml = <<~YAML
+      width: 33mm
+      height: 101mm
+      delta_x: -4mm
+      delta_y: 1cm
+      nlsep: '%%'
+      printer: seiko3
+    YAML
+      setup_test_file('~/.labrat/config', config_yml)
+      hsh = Config.read('labrat', xdg: false, dir_prefix: SANDBOX_DIR)
+      op = ArgParser.new.from_hash(hsh)
+      expect(op.width).to be_within(EPS).of(33 * MM)
+      expect(op.height).to be_within(EPS).of(101 * MM)
+      expect(op.delta_x).to be_within(EPS).of(-4 * MM)
+      expect(op.delta_y).to be_within(EPS).of(1 * CM)
+      expect(op.nlsep).to eq('%%')
+      expect(op.printer).to eq('seiko3')
+    end
+
+    it 'can read from classic system and user config files' do
+      sys_config_yml = <<~YAML
+      width: 33mm
+      height: 101mm
+      delta_x: -4mm
+      delta_y: 1cm
+      nlsep: '%%'
+      printer: seiko3
+    YAML
+      ENV['LABRAT_SYS_CONFIG'] = '/etc/labrat/config.yaml'
+      setup_test_file(ENV['LABRAT_SYS_CONFIG'], sys_config_yml)
+
+      usr_config_yml = <<~YAML
+      height: 102mm
+      delta_x: -7mm
+      delta_y: +30mm
+      nlsep: '++'
+    YAML
+      setup_test_file('~/.labrat/config.yml', usr_config_yml)
+      hsh = Config.read('labrat', xdg: false, dir_prefix: SANDBOX_DIR)
+      op = ArgParser.new.from_hash(hsh)
+      expect(op.width).to be_within(EPS).of(33 * MM)
+      expect(op.height).to be_within(EPS).of(102 * MM)
+      expect(op.delta_x).to be_within(EPS).of(-7 * MM)
+      expect(op.delta_y).to be_within(EPS).of(3 * CM)
+      expect(op.nlsep).to eq('++')
+      expect(op.printer).to eq('seiko3')
+    end
   end
 end
