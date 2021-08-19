b492a9f846d9cda0d1bf024b589f1e1cc79460fd
Daniel E. Doherty <ded-law@ddoherty.net>
2021-08-19 07:16:03 -0500

    Add ability to read labels from standard input.

--- lib/labrat/read_files.rb
+++ lib/labrat/read_files.rb
@@ -1,34 +1,38 @@
 module Labrat
   def self.read_label_texts(fname, nlsep)
-    ofname = fname
-    fname = File.expand_path(fname)
-    unless File.readable?(fname)
-      raise "Cannot open label file '#{ofname}' for reading"
-    end
+    file =
+      if fname
+        ofname = fname
+        fname = File.expand_path(fname)
+        unless File.readable?(fname)
+          raise "Cannot open label file '#{ofname}' for reading"
+        end
+        File.open(fname)
+      else
+        $stdin
+      end
 
     texts = []
-    File.open(fname) do |f|
-      label = nil
-      f.each do |line|
-        next if line =~ /\A#/
+    label = nil
+    file.each do |line|
+      next if line =~ /\A#/
 
-        if line =~ /\A\s*\z/
-          # At blank line record any accumulated label into texts, but remove
-          # the nlsep from the end.
-          if label
-            texts << label.sub(/#{Regexp.quote(nlsep)}\z/, '')
-            label = nil
-          end
-        else
-          # Append a non-blank line to the current label, creating it if
-          # necessary.
-          label ||= ''
-          label << line.chomp + nlsep
+      if line =~ /\A\s*\z/
+        # At blank line record any accumulated label into texts, but remove
+        # the nlsep from the end.
+        if label
+          texts << label.sub(/#{Regexp.quote(nlsep)}\z/, '')
+          label = nil
         end
+      else
+        # Append a non-blank line to the current label, creating it if
+        # necessary.
+        label ||= ''
+        label << line.chomp + nlsep
       end
-      # Last label in the file.
-      texts << label.sub(/#{Regexp.quote(nlsep)}\z/, '') if label
     end
+    # Last label in the file.
+    texts << label.sub(/#{Regexp.quote(nlsep)}\z/, '') if label
     texts
   end
 end
