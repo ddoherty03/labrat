5dc3b1cb970c9124a3a49aa017e4ba3c6e88ca86
Daniel E. Doherty <ded-law@ddoherty.net>
2021-08-25 07:19:29 -0500

    Implement grid-style label printing in Label#generate.

--- lib/labrat/label.rb
+++ lib/labrat/label.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'pry'
+require 'debug'
 
 module Labrat
   class Label
@@ -22,54 +23,52 @@ module Labrat
       # the convention that the margins are named assuming a portrait
       # orientation and swap them here so that when Prawn swaps them again,
       if layout == :portrait
-        tm = ops.top_margin
-        bm = ops.bottom_margin
-        lm = ops.left_margin
-        rm = ops.right_margin
+        tpm = ops.top_page_margin
+        bpm = ops.bottom_page_margin
+        lpm = ops.left_page_margin
+        rpm = ops.right_page_margin
       else
-        lm = ops.top_margin
-        rm = ops.bottom_margin
-        tm = ops.left_margin
-        bm = ops.right_margin
+        lpm = ops.top_page_margin
+        rpm = ops.bottom_page_margin
+        tpm = ops.left_page_margin
+        bpm = ops.right_page_margin
       end
 
-      Prawn::Document.generate(ops.out_file, page_size: [ops.width, ops.height],
-                               left_margin: lm, right_margin: rm,
-                               top_margin: tm, bottom_margin: bm,
+      Prawn::Document.generate(ops.out_file, page_size: [ops.page_width, ops.page_height],
+                               left_margin: lpm, right_margin: rpm,
+                               top_margin: tpm, bottom_margin: bpm,
                                page_layout: layout) do |pdf|
-        # We are setting up the box within which the text of the label will be
-        # printed.  Its width should be reduced by the side margins and its
-        # height by the top and bottom margins.  Since the use specifies label
-        # "width" and "height" using the portrait layout, we need to swap
-        # those if a landscape orientation is used.
-        box_wd = (ops.landscape ? ops.height : ops.width) - lm - rm
-        box_ht = (ops.landscape ? ops.width : ops.height) - tm - bm
-        # The first parameter to bounding_box is the top-left corner of the
-        # bounding box relative to the margin box set up for the page. The
-        # user can push this around with delta_x and delta_y (within the
-        # limits of the printer) if the peculiarities of the printer require
-        # it.
-        box_x = 0.mm + ops.delta_x
-        box_y = box_ht + ops.delta_y
-        if ops.verbose
-          warn "orientation: #{layout}"
-          warn "page_size = [#{ops.width}pt,#{ops.height}pt]"
-          warn "[lm, rm] = [#{lm}pt,#{rm}pt]"
-          warn "[tm, bm] = [#{tm}pt,#{bm}pt]"
-          warn "[delta_x, delta_y] = [#{ops.delta_x}pt,#{ops.delta_y}pt]"
-          warn "[box_x, box_y] = [#{box_x}pt,#{box_y}pt]"
-          warn "[box_wd, box_ht] = [#{box_wd}pt,#{box_ht}pt]"
-        end
+        # Define a grid with each grid box to be used for a single label.
+        pdf.define_grid(rows: ops.rows, columns: ops.columns,
+                        row_gutter: ops.row_gap, column_gutter: ops.column_gap)
+        # if ops.verbose
+        #   warn "orientation: #{layout}"
+        #   warn "page_size = [#{ops.width}pt,#{ops.height}pt]"
+        #   warn "[lpm, rpm] = [#{lpm}pt,#{rpm}pt]"
+        #   warn "[tpm, bpm] = [#{tpm}pt,#{bpm}pt]"
+        #   warn "[delta_x, delta_y] = [#{ops.delta_x}pt,#{ops.delta_y}pt]"
+        #   warn "[box_x, box_y] = [#{box_x}pt,#{box_y}pt]"
+        #   warn "[box_wd, box_ht] = [#{box_wd}pt,#{box_ht}pt]"
+        # end
+        # pdf.grid.show_all
         last_k = texts.size - 1
         texts.each_with_index do |text, k|
-          pdf.bounding_box([box_x, box_y], width: box_wd, height: box_ht) do
+          row, col = row_col(k + 1)
+          pdf.grid(row, col).bounding_box do
+            bounds = pdf.bounds
+            box_wd = (bounds.right - bounds.left) - ops.left_margin - ops.right_margin
+            box_ht = (bounds.top - bounds.bottom) - ops.top_margin - ops.bottom_margin
+            box_x = ops.left_margin + ops.delta_x
+            box_y = ops.bottom_margin + box_ht + ops.delta_y
+            # pdf.bounding_box([box_x, box_y], width: box_wd, height: box_ht) do
             pdf.stroke_bounds
             pdf.font ops.font_name, style: ops.font_style.to_sym, size: ops.font_size.to_f
             pdf.text_box(text, width: box_wd, height: box_ht,
                          align: ops.h_align, valign: ops.v_align,
-                         overflow: :truncate, at: [0, box_ht])
+                         overflow: :truncate, at: [box_x, box_y])
+            # binding.break if k == 0
+            pdf.start_new_page if needs_new_page?(k, last_k)
           end
-          pdf.start_new_page unless k == last_k
         end
       end
       self
