18c5371eef6216c23da851551e2183bfc433e64f
Daniel E. Doherty <ded-law@ddoherty.net>
2021-09-24 06:42:54 -0500

    Add expectation for nil error message in ArgParser spec.

--- spec/arg_parser_spec.rb
+++ spec/arg_parser_spec.rb
@@ -125,14 +125,16 @@ RSpec.describe ArgParser do
 
     it 'produces help on invalid option' do
       bad_option = '--lsflkwroi'
-      err_msg = ap.parse([bad_option]).msg
+      ops = ap.parse([bad_option])
+      err_msg = ops.msg
       expect(err_msg).to include('Error: invalid option')
       expect(err_msg).to include(bad_option)
     end
 
     it 'produces help on missing option arg' do
       option = '--delta_x'
-      err_msg = ap.parse([option]).msg
+      ops = ap.parse([option])
+      err_msg = ops.msg
       expect(err_msg).to include('Error: missing argument:')
       expect(err_msg).to include(option)
     end
@@ -140,14 +142,17 @@ RSpec.describe ArgParser do
     it 'can set the dimensions of the page' do
       # Points
       ops = ap.parse(['-w', '44', '-h', '180.5'])
+      expect(ops.msg).to be_nil
       expect(ops.page_width).to be_within(EPS).of(44.0)
       expect(ops.page_height).to be_within(EPS).of(180.5)
       # Long options
       ops = ap.parse(['--page-width=4cm', '--page-height', '60mm'])
+      expect(ops.msg).to be_nil
       expect(ops.page_width).to be_within(EPS).of(40 * MM)
       expect(ops.page_height).to be_within(EPS).of(60 * MM)
       # Short options
       ops = ap.parse(['-w', '0.5in', '-h', '3.4375in'])
+      expect(ops.msg).to be_nil
       expect(ops.page_width).to be_within(EPS).of(0.5 * IN)
       expect(ops.page_height).to be_within(EPS).of(3.4375 * IN)
     end
@@ -155,14 +160,18 @@ RSpec.describe ArgParser do
     it 'can set the name of a label' do
       LabelDb['dymo30327'] = {page_width: 18}
       ops = ap.parse(['--label=dymo30327'])
+      expect(ops.msg).to be_nil
       expect(ops.label).to eq('dymo30327')
     end
 
     it 'can set the offset dimensions' do
       ops = ap.parse(['--delta_x=0.5cm', '--delta_y', '6mm'])
+      expect(ops.msg).to be_nil
       expect(ops.delta_x).to be_within(EPS).of(5 * MM)
       expect(ops.delta_y).to be_within(EPS).of(6 * MM)
+
       ops = ap.parse(['-x', '0.5in', '-y', '3.4375in'])
+      expect(ops.msg).to be_nil
       expect(ops.delta_x).to be_within(EPS).of(0.5 * IN)
       expect(ops.delta_y).to be_within(EPS).of(3.4375 * IN)
     end
@@ -172,6 +181,7 @@ RSpec.describe ArgParser do
       units = %i[pt mm cm dm m in ft yd]
       units.each do |unit|
         ops = ap.parse(["--delta_x=5#{unit}"])
+        expect(ops.msg).to be_nil
         expect(ops.delta_x).to be_kind_of(Numeric)
       end
       units = %i[mi furlongs]
@@ -182,88 +192,125 @@ RSpec.describe ArgParser do
 
     it 'can set the printer name' do
       ops = ap.parse(['--printer=epson55'])
-      expect(ops.printer).to eq('epson55')
-    end
-
-    it 'can set the printer name' do
-      ops = ap.parse(['--printer=epson55'])
+      expect(ops.msg).to be_nil
       expect(ops.printer).to eq('epson55')
     end
 
     it 'can set the new-line marker' do
       ops = ap.parse(['--nlsep=&&'])
+      expect(ops.msg).to be_nil
       expect(ops.nlsep).to eq('&&')
       ops = ap.parse(['-n', '&&'])
+      expect(ops.msg).to be_nil
       expect(ops.nlsep).to eq('&&')
     end
 
     it 'can set an optional input file' do
       ops = ap.parse(['-f junk.lab'])
+      expect(ops.msg).to be_nil
       expect(ops.file).to eq('junk.lab')
-      ops = ap.parse(['--file junk.lab'])
-      expect(ops.file).to eq('junk.lab')
+      ops = ap.parse(['--file=junk2.lab'])
+      expect(ops.msg).to be_nil
+      expect(ops.file).to eq('junk2.lab')
       ops = ap.parse(['--file', '  file with some spaces  	'])
+      expect(ops.msg).to be_nil
       expect(ops.file).to eq('file with some spaces')
     end
 
     it 'can set an optional output PDF file' do
       ops = ap.parse(['-ojunk'])
+      expect(ops.msg).to be_nil
       expect(ops.out_file).to eq('junk.pdf')
-      ops = ap.parse(['--out-file junk'])
+
+      ops = ap.parse(['--out-file=junk'])
+      expect(ops.msg).to be_nil
       expect(ops.out_file).to eq('junk.pdf')
+
       ops = ap.parse(['--out-file=junk.PDF'])
+      expect(ops.msg).to be_nil
       expect(ops.out_file).to eq('junk.PDF')
+
       ops = ap.parse(['--out-file', '  file with some spaces  	'])
+      expect(ops.msg).to be_nil
       expect(ops.out_file).to eq('file with some spaces.pdf')
     end
 
     it 'can set an optional print command' do
       ops = ap.parse(["-%lppr -P %p %o"])
+      expect(ops.msg).to be_nil
       expect(ops.print_command).to eq('lppr -P %p %o')
+
       ops = ap.parse(["--print-command=lppr -P %p %o"])
+      expect(ops.msg).to be_nil
       expect(ops.print_command).to eq('lppr -P %p %o')
     end
 
     it 'can set an optional view command' do
       ops = ap.parse(["-:snapview %o"])
+      expect(ops.msg).to be_nil
       expect(ops.view_command).to eq('snapview %o')
+
       ops = ap.parse(["--view-command=snapview %o"])
+      expect(ops.msg).to be_nil
       expect(ops.view_command).to eq('snapview %o')
     end
 
     it 'can ask to view rather than print' do
       ops = ap.parse(["-V"])
+      expect(ops.msg).to be_nil
       expect(ops.view).to be true
+
       ops = ap.parse(["--view"])
+      expect(ops.msg).to be_nil
       expect(ops.view).to be true
+
       ops = ap.parse(["--no-view"])
+      expect(ops.msg).to be_nil
       expect(ops.view).to be false
     end
 
     it 'can set orientation' do
       ops = ap.parse(['-L'])
+      expect(ops.msg).to be_nil
       expect(ops.landscape).to be true
+
       ops = ap.parse(['--landscape'])
+      expect(ops.msg).to be_nil
       expect(ops.landscape).to be true
+
       ops = ap.parse(['--no-landscape'])
+      expect(ops.msg).to be_nil
       expect(ops.landscape).to be false
+
       # Portrait is, essentially, --no-landacape
       ops = ap.parse(['-P'])
+      expect(ops.msg).to be_nil
       expect(ops.landscape).to be false
+
       ops = ap.parse(['--portrait'])
+      expect(ops.msg).to be_nil
       expect(ops.landscape).to be false
+
       ops = ap.parse(['--no-portrait'])
+      expect(ops.msg).to be_nil
       expect(ops.landscape).to be true
     end
 
     it 'can set verbose' do
       ops = ap.parse([])
+      expect(ops.msg).to be_nil
       expect(ops.verbose).to be false
+
       ops = ap.parse(['-v'])
+      expect(ops.msg).to be_nil
       expect(ops.verbose).to be true
+
       ops = ap.parse(['--verbose'])
+      expect(ops.msg).to be_nil
       expect(ops.verbose).to be true
+
       ops = ap.parse(['--no-verbose'])
+      expect(ops.msg).to be_nil
       expect(ops.verbose).to be false
     end
   end
